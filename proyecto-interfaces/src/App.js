7d5e9520a5b2b0a30bb939bbc27dd401d601f999
TheTenAreOne <daniel.caicedoj@gmail.com>
2021-09-01 21:30:30 -0500

    clean App.js

--- proyecto-interfaces/src/App.js
+++ proyecto-interfaces/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path='/'>
+          You're at home path
+          <Link to='/vuelos'>Go to vuelos</Link>
+        </Route>
+        <Route exact path='/vuelos'>
+          You're at vuelos path
+          <Link to='/'>Go to home</Link>
+        </Route>
+      </Switch>
     </div>
   );
 }
