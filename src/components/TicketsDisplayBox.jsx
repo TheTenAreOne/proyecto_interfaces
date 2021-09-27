c0bfa469b2382454e2e60dd47234dd9e0246479e
TheTenAreOne <daniel.caicedoj@gmail.com>
2021-09-27 08:55:21 -0500

    add TicketsDisplayBox

---
+++ src/components/TicketsDisplayBox.jsx
@@ -0,0 +1,14 @@
+
+import '../pages/Home.css';
+
+function TicketsDisplayBox(props){
+  return(
+    <div className='container ticketsDisplayBox'>
+      <div>
+        Llenen esto de tickets con bucle al presionar buscar
+      </div>
+    </div>
+  );
+}
+
+export default TicketsDisplayBox;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,21 @@ import { useEffect } from 'react';
 import MainNavbar from '../components/MainNavbar';
 import OneOptionButton from '../components/OneOptionButton';
 import TicketOptionsBox from '../components/TicketOptionsBox';
+import TicketsDisplayBox from '../components/TicketsDisplayBox';
 import './Home.css'
 function Home() {
   return(
     <div className='container'>
       <div className='row'>
-        <div className='col'>
+        <div className='col' id='MainNavbar'>
           <MainNavbar/>
         </div>
       </div>
-      <div className='row'>
+      <div className='row' id='TicketOptionsBox'>
         <TicketOptionsBox/>
+      </div>
+      <div className='row' id='TicketsDisplayBox'>
+        <TicketsDisplayBox />
       </div>
     </div>
   );
