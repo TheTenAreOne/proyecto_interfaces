5de0148bb2280bc6ecd11eb1cdbb45f931e79371
TheTenAreOne <daniel.caicedoj@gmail.com>
2021-09-28 14:51:53 -0500

    add style to ticket display

--- src/components/TicketInfoBox.jsx
+++ src/components/TicketInfoBox.jsx
@@ -4,23 +4,25 @@ import '../pages/Home.css';
 function TicketInfoBox(props) {
   console.log(props);
   return (
+    <a className='ticketButtonBox' href='/'>
     <div className="container ticketBox">
       <div className='row'>
-        <div className='col-5'>{props.ticketInfo.cityOrigin}</div>
+        <div className='col-5 ticketCity'>{props.ticketInfo.cityOrigin}</div>
         <div className='col-2'>
-          <div className=''>&#129050;</div>
+          <div className='ticketArrow'>&#129050;</div>
         </div>
-        <div className='col-5'>{props.ticketInfo.cityDestination}</div>
+        <div className='col-5 ticketCity'>{props.ticketInfo.cityDestination}</div>
       </div>
       <div className='row'>
-        <div className='col-5'>{props.ticketInfo.takeoff}</div>
-        <div className='col-2'>Escalas: {props.ticketInfo.scales}</div>
-        <div className='col-5'>{props.ticketInfo.landing}</div>
+        <div className='col-4'>{props.ticketInfo.takeoff}</div>
+        <div className='col-4'>Escalas: {props.ticketInfo.scales}</div>
+        <div className='col-4'>{props.ticketInfo.landing}</div>
       </div>
       <div className='row'>
-        <div>${props.ticketInfo.price}</div>
+        <div className='ticketPrice'>${props.ticketInfo.price}</div>
       </div>
     </div>
+    </a>
   )
 }
 
